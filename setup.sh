2014f8c1c64b96b2e1ce2e568b135a9743f8362e
kesompochy <kesompochy@gmail.com>
2024-01-08 16:00:30 +0900

    installをOSごとにわけた

--- setup.sh
+++ setup.sh
@@ -1,8 +1,21 @@
-echo "Start installing packages..."
-sudo apt-get install neovim
-sudo apt-get install fzf
-go install github.com/x-motemen/ghq@latest
-echo "Finished installing packages!"
+#!/usr/bin/env fish
+echo "===Start installing packages...==="
 
-echo "Start linking config files..."
-echo "Finished linking config files!"
+echo "Install packages for "(uname)"!!"
+switch (uname)
+  case Linux
+    sudo apt-get install neovim
+    sudo apt-get install fzf
+
+  case Darwin
+    brew install neovim
+    brew install fzf
+end
+
+echo "Install common packages!!"
+go install github.com/x-motemen/ghq@latest
+
+echo "===Finished installing packages!==="
+
+echo "===Start linking config files...==="
+echo "===Finished linking config files!==="
