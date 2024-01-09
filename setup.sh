9a30e13a1d0f204745768c53872f40336f788c18
kesompochy <kesompochy@gmail.com>
2024-01-09 17:25:18 +0900

    .config以下ぜんぶ自動でリンクする

--- setup.sh
+++ setup.sh
@@ -18,16 +18,20 @@ go install github.com/x-motemen/ghq@latest
 echo "===Finished installing packages!==="
 
 echo "===Start linking config files...==="
-if test -e $HOME/.config/nvim
-  set backup_dir "$HOME/.config/nvim_backup_"(date +%Y%m%d)
-  mv $HOME/.config/nvim $backup_dir
-  echo "Backed up existing configs, $backup_dir"
+set repo_config_dir (pwd)/.config
+set home_config_dir $HOME/.config
+
+for dir in (ls $repo_config_dir)
+  set repo_subdir $repo_config_dir/$dir
+  set home_subdir $home_config_dir/$dir
+  echo "Linking $repo_subdir to $home_subdir!!"
+
+  if test -e $home_subdir
+    set backup_dir $home_subdir"_backup_"(date +%Y%m%d_%H%M%S)
+    mv $home_subdir $backup_dir
+    echo "Backed up existing configs to $backup_dir!!"
+  end
+  ln -s $repo_subdir $home_subdir
+  echo "Linked $repo_subdir to $home_subdir!!"
 end
-ln -s (pwd)/.config/nvim $HOME/.config/nvim
-if test -e $HOME/.config/fish
-  set backup_dir "$HOME/.config/fish_backup_"(date +%Y%m%d)
-  mv $HOME/.config/fish $backup_dir
-  echo "Backed up existing configs, $backup_dir"
-end
-ln -s (pwd)/.config/fish $HOME/.config/fish
 echo "===Finished linking config files!==="
