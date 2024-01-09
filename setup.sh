65a13908742f7c45d9d78b849f048e870e6b8d4b
kesompochy <kesompochy@gmail.com>
2024-01-09 17:13:53 +0900

    symlink貼るようにした

--- setup.sh
+++ setup.sh
@@ -18,4 +18,16 @@ go install github.com/x-motemen/ghq@latest
 echo "===Finished installing packages!==="
 
 echo "===Start linking config files...==="
+if test -e $HOME/.config/nvim
+  set backup_dir "$HOME/.config/nvim_backup_"(date +%Y%m%d)
+  mv $HOME/.config/nvim $backup_dir
+  echo "Backed up existing configs, $backup_dir"
+end
+ln -s (pwd)/.config/nvim $HOME/.config/nvim
+if test -e $HOME/.config/fish
+  set backup_dir "$HOME/.config/fish_backup_"(date +%Y%m%d)
+  mv $HOME/.config/fish $backup_dir
+  echo "Backed up existing configs, $backup_dir"
+end
+ln -s (pwd)/.config/fish $HOME/.config/fish
 echo "===Finished linking config files!==="
