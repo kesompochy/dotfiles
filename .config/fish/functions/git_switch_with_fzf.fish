4d34fa27a31b536aaf04b893b3260a83ab74dd57
kesompochy <kesompochy@gmail.com>
2024-03-06 20:55:38 +0900

    ないブランチも指定できるようにした

--- .config/fish/functions/git_switch_with_fzf.fish
+++ .config/fish/functions/git_switch_with_fzf.fish
@@ -1,8 +1,20 @@
 function git_switch_with_fzf
-  set branch (git branch --all | fzf)
+  set dummy "  New branch!!"
+  set branch (begin; echo $dummy; git branch --all; end | fzf)
 
   if test -n "$branch"
-    git switch (echo $branch | sed 's/.* //')
+    if test "$branch" = "$dummy"
+      echo "Enter the new branch name:"
+      read -l new_branch
+      if git branch --all | grep -q $new_branch
+        echo "Branch $new_branch already exists"
+        git switch $new_branch
+      else
+        git switch -c $new_branch
+      end
+    else
+      git switch (echo $branch | sed 's/.* //')
+    end
   else 
     echo "No branch selected"
   end
