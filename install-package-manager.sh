80e851025171ccef6a679c0608248543b6113019
kesompochy <kesompochy@gmail.com>
2024-03-10 06:15:01 +0900

    シェルスクリプトが普通にミスってた

--- install-package-manager.sh
+++ install-package-manager.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [! -f ~/.local/share/nvim/site/autoload/plug.vim]; then
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
   echo "Installing vim-plug..."
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
