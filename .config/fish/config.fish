198a3faf3170fec05d82d77e5781c63e8b2b65af
kesompochy <kesompochy@gmail.com>
2024-03-10 05:58:01 +0900

    wsl2用の時刻同期設定をfishに仕込んだ

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,11 @@ set -x BUN_INSTALL $HOME/.bun
 set -x PATH $BUN_INSTALL/bin:$PATH
 
 set -x WIN_CHROME_PATH /mnt/c/'Program Files'/Google/Chrome/Application/chrome.exe
+
+# wsl2の時刻同期コマンド
+if grep -q microsoft /proc/version
+  if test -d "/run/WSL"
+    echo "WSL2 detected, setting up time sync..."
+    sudo hwclock --hctosys
+  end
+end
