455ecabb2702ab9ddf94e96dd34ac1c38cb3b387
kesompochy <kesompochy@gmail.com>
2024-04-13 23:28:51 +0900

    nodenvの設定を追加した

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,4 +13,4 @@ set -x PATH $BUN_INSTALL/bin:$PATH
 set -x WIN_CHROME_PATH /mnt/c/'Program Files'/Google/Chrome/Application/chrome.exe
 
 set -x PATH $HOME/.nodenv/bin $PATH
-status --is-interactive; and source (nodenv init -|psub)
+nodenv init - | source
