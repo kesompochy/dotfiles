50b7252d697966c0a1348ac686a257d2f70bf399
kesompochy <kesompochy@gmail.com>
2025-01-07 06:20:48 +0900

    use fish_add_path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,21 +3,28 @@
 ## TODO
 
 # path
+set -e fish_user_paths
 
 set -x GOPATH $HOME/go
-set -x PATH $PATH $GOPATH/bin /opt/homebrew/bin
+fish_add_path $GOPATH/bin
+fish_add_path /opt/homebrew/bin
 
 set -x BUN_INSTALL $HOME/.bun
-set -x PATH $BUN_INSTALL/bin:$PATH
+fish_add_path $BUN_INSTALL/bin
 
 set -x WIN_CHROME_PATH /mnt/c/'Program Files'/Google/Chrome/Application/chrome.exe
 
-set -x PATH $HOME/.nodenv/bin $PATH
+fish_add_path $HOME/.nodenv/bin
 nodenv init - | source
 
-set -x PATH $HOME/.tfenv/bin:$PATH
+fish_add_path $HOME/.tfenv/bin
+
+fish_add_path /usr/lib/llvm16/bin
 
 # pyenv setup
 set -x PYENV_ROOT $HOME/.pyenv
 fish_add_path $PYENV_ROOT/bin
 pyenv init - | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/kesompochy/google-cloud-sdk/path.fish.inc' ]; . '/home/kesompochy/google-cloud-sdk/path.fish.inc'; end
