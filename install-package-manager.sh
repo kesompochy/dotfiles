c1c197fd55f323bd847bdb102cd48caeb641637d
kesompochy <kesompochy@gmail.com>
2025-08-14 16:24:16 +0900

    nvmが推奨されている

--- install-package-manager.sh
+++ install-package-manager.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+# Install nvm and Node.js
+if [ ! -d "$HOME/.nvm" ]; then
+  echo "Installing nvm..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  echo "nvm installed!"
+else
+  echo "nvm already installed"
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+fi
+
+# Install Node.js LTS if not installed
+if ! nvm ls --no-colors | grep -q "lts"; then
+  echo "Installing Node.js LTS..."
+  nvm install --lts
+  nvm use --lts
+  nvm alias default lts/*
+  echo "Node.js LTS installed!"
+else
+  echo "Node.js LTS already installed"
+fi
+
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
   echo "Installing vim-plug..."
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
