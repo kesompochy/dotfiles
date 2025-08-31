f8cc00df4f42a043d24fc3407ba92b2be10a23d8
kesompochy <kesompochy@gmail.com>
2025-08-31 18:07:46 +0900

    workspace-writeじゃないとworkspace-writeの中の設定は反映されない

--- .config/fish/functions/codex-net.fish
+++ .config/fish/functions/codex-net.fish
@@ -1,4 +1,4 @@
 function codex-net --wraps codex --description 'Codex with network access enabled in workspace-write sandbox'
-  command codex --config 'sandbox_workspace_write.network_access=true' $argv
+  command codex --config 'sandbox_mode="workspace-write"' --config 'sandbox_workspace_write.network_access=true' $argv
 end
 
